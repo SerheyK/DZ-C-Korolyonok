0734999da7921a1953d725822dfaef68d3cbe3c2
SerheyK <korolyonok@inbox.ru>
2022-11-17 17:47:09 +0300

    Решение задачи №1!

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите первое число");
+string str = Console.ReadLine();
+int numb1 = int.Parse(str);
+Console.WriteLine("Введите второе число");
+str = Console.ReadLine();
+int numb2 = int.Parse(str);
+if (numb1>numb2)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else
+{
+Console.WriteLine("Второе число больше первого");
+}
